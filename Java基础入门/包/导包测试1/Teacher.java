2941021857eb00b6f00b4a34ecc88bae4df56b29
programmerXSX <89210461+programmerXSX@users.noreply.github.com>
2021-08-20 13:20:10 +0800

    Add files via upload

---
+++ Java基础入门/包/导包测试1/Teacher.java
@@ -0,0 +1,10 @@
+package 包.导包测试1;
+
+public class Teacher {
+    int age = 30;
+    String name = "xsx";
+    public void show(){
+        System.out.println("老师年龄：" + age);
+        System.out.println("老师名字：" + name);
+    }
+}
