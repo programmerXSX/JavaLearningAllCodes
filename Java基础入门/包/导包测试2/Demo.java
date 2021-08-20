db104f0da8ea8858982e358a1ca7c81904de998a
programmerXSX <89210461+programmerXSX@users.noreply.github.com>
2021-08-20 13:22:11 +0800

    Add files via upload

---
+++ Java基础入门/包/导包测试2/Demo.java
@@ -0,0 +1,11 @@
+package 包.导包测试2;
+import 包.导包测试1.*;
+
+public class Demo {
+    public static void main(String[] args) {
+        Teacher t = new Teacher();
+        t.show();
+        Student s = new Student();
+        s.show();
+    }
+}
